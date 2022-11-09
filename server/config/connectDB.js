1c758b21cc43c4f61d6a0e163e0a3301b0ac7870
fiskryeziu <fiskryeziu@gmail.com>
2022-11-09 21:31:33 +0100

    change mongodb from local to atlas

--- server/config/connectDB.js
+++ server/config/connectDB.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose'
 
 const connectDb = async () => {
   try {
-    const connect = await mongoose.connect('mongodb://localhost:27017/car')
+    const connect = await mongoose.connect(process.env.MONGO_URI)
     console.log(`MongoDb server started: ${connect.connection.host}`)
   } catch (error) {
     console.error(`Error ${error.message}`)
